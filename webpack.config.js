90b811b779b30ecccca09a73f9453b4dd67e5f5c
younhoso <thdbsgh3443@naver.com>
2019-08-08 20:19:54 +0900

    190808 커밋

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,18 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['es2015', 'react', {modules: false}]
+          ]
+        }
+      }]
+    }]
+  },
 };
