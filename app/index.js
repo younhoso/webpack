74fef120b8d5a6c834c7c2dabcb87e74cdfb8fe3
younhoso <thdbsgh3443@naver.com>
2019-08-08 17:30:07 +0900

    190808 커밋

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
+
 import _ from 'lodash';
+import $ from 'jquery';
 
 function component () {
   var element = document.createElement('div');
 
-  /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
+  $(element).html( _.join(['Hello','webpack'], ' '));
 
   return element;
 }
 
-document.getElementById('body-content').appendChild(component());
+document.body.appendChild(component());
